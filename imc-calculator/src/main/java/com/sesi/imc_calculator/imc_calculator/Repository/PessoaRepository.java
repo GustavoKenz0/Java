1aa17f1864a75b2e8e35dbee9326744032d54736
Kenzo <146833859+GustavoKenz0@users.noreply.github.com>
2024-09-04 11:46:50 -0300

    Add files via upload

---
+++ imc-calculator/src/main/java/com/sesi/imc_calculator/imc_calculator/Repository/PessoaRepository.java
@@ -0,0 +1,9 @@
+package com.sesi.imc_calculator.imc_calculator.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sesi.imc_calculator.imc_calculator.model.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+	
+}
